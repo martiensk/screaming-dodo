83a60478fd4b583ede48021600dcd1843833d55f
Martiens Kropff <martiens@annexio.com>
2024-09-13 13:50:10 +0100

    testing pre-commit

---
+++ components/templates/Default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="banner-image hero h-96" :style="bgImageStyle">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-md">
+                <h1 class="mb-5 text-5xl font-bold">{{ config.name }}</h1>
+                <p class="mb-5">
+                    {{ config.description }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+    import config from '~/config.json';
+    import { defineProps } from 'vue';
+
+    const bgImageStyle = {
+        backgroundImage: `url(/images/${config.bannerImage})`
+    };
+</script>
