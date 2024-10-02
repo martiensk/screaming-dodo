9a65783cbed95350bc045546dd25e5122237d758
Martiens Kropff <martiensk@gmail.com>
2024-10-02 13:35:29 +0100

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -1,44 +1,54 @@
 export default defineAppConfig({
-    theme: {
-        colourScheme: 'light',
-        homeTemplate: 'default',
-        forceConfig: true
-    },
+  theme: {
+      colourScheme: 'light',
+      homeTemplate: 'default',
+      forceConfig: true
+  },
 
-    categories: {
-        categories: [
-            {
-                name: 'Health',
-                description: 'Health related articles',
-                image: 'health.webp'
-            },
-            {
-                name: 'Technology',
-                description: 'Technology related articles',
-                image: 'tech.webp'
-            },
-            {
-                name: 'Fashion',
-                description: 'Fashion related articles',
-                image: 'fashion.png'
-            },
-            {
-                name: 'Science',
-                description: 'Science related articles',
-                image: 'science.png'
-            }
-        ]
-    },
+  categories: {
+      categories: [
+          {
+              name: 'Health',
+              description: 'Health related articles',
+              image: 'health.webp'
+          },
+          {
+              name: 'Technology',
+              description: 'Technology related articles',
+              image: 'tech.webp'
+          },
+          {
+              name: 'Fashion',
+              description: 'Fashion related articles',
+              image: 'fashion.png'
+          },
+          {
+              name: 'Science',
+              description: 'Science related articles',
+              image: 'science.png'
+          }
+      ]
+  },
 
-    blog: {
-        name: 'Screaming Dodo',
-        description: 'A simple and clean theme for Ghost',
-        summary: 'A simple and clean theme for Ghost, but not the band. Something else entirely.',
-        bannerImage: 'bannerimage.jpg',
-        maxArticles: 20
-    },
+  blog: {
+      name: 'Screaming Dodo',
+      description: 'A simple and clean theme for Ghost',
+      summary: 'A simple and clean theme for Ghost, but not the band. Something else entirely.',
+      bannerImage: 'bannerimage.jpg',
+      maxArticles: 20
+  },
 
-    seo: {
-        logo: 'logo.png'
-    }
+  seo: {
+      logo: 'logo.png'
+  },
+
+  site: {
+    logo: 'logo.png',
+    url: 'http:/screamingdodo.com',
+    name: 'Screaming Dodo',
+    description: 'A simple and clean theme for Ghost',
+    summary: 'A simple and clean theme for Ghost, but not the band. Something else entirely.',
+    bannerImage: 'bannerimage.jpg',
+    maxArticles: 20
+  }
 });
