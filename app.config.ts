8c6fb35cb62bd4542db8efdbce19994e435aed11
Martiens Kropff <martiensk@gmail.com>
2024-10-14 13:24:35 +0100

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -12,7 +12,7 @@ export default defineAppConfig({
     site: {
         logo: 'logo.png',
         url: 'https://www.screamingdodo.com',
-        name: 'The Screaming DodoScreaming',
+        name: 'The Screaming Dodo',
         description: 'Where the grave meets the grin',
         summary: 'Step into the shadowy world of the Screaming Dodo,  where the macabre dances with mirth and the eerie mingles with the entertaining. Our blog is a unique sanctuary for those who find fascination in the darker corners of existence but prefer their doom and gloom served with a side of levity.',
         bannerImage: 'banner.jpeg',
