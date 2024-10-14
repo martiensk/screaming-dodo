2473c39c07b01724d1966c17a62530e787b9704e
Martiens Kropff <martiensk@gmail.com>
2024-10-14 13:07:03 +0100

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -6,11 +6,7 @@ export default defineAppConfig({
     },
 
     categories: {
-        categories: [{
-          name: 'Cults',
-          description: 'Sinister cults!',
-          image: 'cults.jpg'
-        }]
+        categories: [null]
     },
 
     site: {
@@ -18,7 +14,7 @@ export default defineAppConfig({
         url: 'https://www.screamingdodo.com',
         name: 'The Screaming DodoScreaming',
         description: 'Where the grave meets the grin',
-        summary: 'Where the grave meets the grin',
+        summary: 'Step into the shadowy world of the Screaming Dodo,  where the macabre dances with mirth and the eerie mingles with the entertaining. Our blog is a unique sanctuary for those who find fascination in the darker corners of existence but prefer their doom and gloom served with a side of levity.',
         bannerImage: 'banner.jpeg',
         maxArticles: 20
     }
